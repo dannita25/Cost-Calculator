0943aa3e350eb5bbc3ed929858ed10a7f4783a42
dannita25 <101114780+dannita25@users.noreply.github.com>
2024-12-06 12:04:44 +0000

    Update script.js

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ let averageSalary = 34963; //***
 let averageAbsenceRate = 2.6 / 100; //***
 let mskRate = 21 / 100; //*** 
 let reductionRate = 20 / 100; 
-let niMultiplier = 1.138; // Updated NI multiplier ***
-let employersPensionContribution = 1.03;//***
+let niMultiplier = 1.15; // Updated NI multiplier ***
+let employersPensionContribution = 0.03;//***
 
 function calculateCost() {
   const employees = parseFloat(document.getElementById('employees').value);
